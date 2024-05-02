5d28e5ef7780b516c641b10c925379090e9d2544
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-05-02 20:48:15 +0100

    add login form

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,55 @@
 import CenterMode from "../../components/ui/carousel-home/carousel-home";
 import PageLayout from "../../components/layouts/page-layout/page-layout";
 import("./login.css")
+import backgroundHome from "../../assets/images/background-home-black.jpg"
+import { useForm } from "react-hook-form"
+import { NavLink } from "react-router-dom";
 
 
 
 
 function Login() {
+
+    const { register, handleSubmit, formState: {errors}} = useForm()
+
+    const handleEventSubmit = (event) => {
+        console.log(event)
+
+
+    }
+
+
     return (
-        <PageLayout>
+        <PageLayout background={backgroundHome}>
+           <div className=" h-100 d-flex justify-content-center align-items-center row" style={{ minHeight: "calc(100vh + 72px)" }}>
+                <form onSubmit={handleSubmit(handleEventSubmit)} className="form-container pt-4 col-8 col-md-6 col-lg-4">
+                    {/*email  */}
+                    <div>
+                        <label className="form-label">Email address</label>
+                        <input {...register("email",{required:"Email is required"})} type="email" className = {`form-control ${errors.email ? "is-invalid" : ""}`} />
+                        {errors.email ? (<div className="invalid-feedback">{errors.email.message} </div>): ""}
+                    </div>
+                    {/*password*/}
+                    <div>
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input {...register("password",{required:"Password is required"})} type="text" className = {`form-control ${errors.password ? "is-invalid" : ""}`} />
+                        {errors.password ? (<div className="invalid-feedback">{errors.password.message} </div>): ""}
+                    </div>
+                
+                    <div className="d-flex justify-content-center">
+                        <div className="col-6">
+                            <button  type="submit" className="btn col-6 btn-danger mt-4 mb-2">Sign in</button>
+                        </div>
+                        <div>
+                            <p className="mt-4">Don't have a count? <NavLink to = "/register">Signup</NavLink></p>
+                        </div>
+                         
+                    
+                    </div>
+                   
+                </form>
+
+            </div>
             
 
         </PageLayout>
